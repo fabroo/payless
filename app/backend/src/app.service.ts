5146bce69ec4f4c359f6f34075e4ff6a5901bda2
Tievoo <tievolib@gmail.com>
2022-05-13 14:43:05 -0300

    feat: 🎸 add helpers & interfaces

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -1,33 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from "@nestjs/axios";
-import { map } from 'rxjs';
+import { UberHelper } from './helpers/uber.helper';
+import { CabifyHelper } from './helpers/cabify.helper';
+import { DidiHelper } from './helpers/didi.helper';
+import { BeatHelper } from './helpers/beat.helper';
+import { Location } from './interfaces/location';
 
 @Injectable()
 export class AppService {
-  constructor(private http: HttpService){}
-  async authorize(code: string): Promise<any> {
-    console.log(code)
+  constructor(
+    private http: HttpService,
+    private uberHelper : UberHelper,
+    private cabifyHelper : CabifyHelper,
+    private didiHelper : DidiHelper,
+    private beatHelper : BeatHelper
+  ){}
+  
+  public async getTripPrices(from: Location, to: Location) {
 
-    console.log(`
-    curl -F 'client_secret=${process.env.UBER_CLIENT_SECRET}' \
-    -F 'client_id=${process.env.UBER_CLIENT_ID}' \
-    -F 'grant_type=authorization_code' \
-    -F 'redirect_uri=http://localhost:4500/redirect_uri' \
-    -F 'scope=profile' \
-    -F 'code=${code}' \
-    https://login.uber.com/oauth/v2/token
-
- `)
-
-
-    // return await this.http.get('https://api.uber.com/v1.2/estimates/price').pipe(map(response => response.data))
-    return await this.http.post('https://login.uber.com/oauth/v2/token', {
-      client_id: process.env.UBER_CLIENT_ID,
-      client_secret: process.env.UBER_SECRET_ID,
-      grant_type: 'authorization_code',
-      redirect_uri: 'http://localhost:4500/redirect_uri',
-      code: code,
-      scope: 'profile'
-    }).pipe(map(response => response.data))
   }
 }
